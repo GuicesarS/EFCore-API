9175c40411e017887b08aca4322fce4904ce7128
Guilherme Soares <guicesarsosil@gmail.com>
2025-05-21 11:19:18 -0300

    feat: implementação da WebAPI com EF Core 8 e Minimal API

---
+++ FuscaFilmes.API/Program.cs
@@ -0,0 +1,79 @@
+using System.Security.Cryptography.Xml;
+using System.Text.Json.Serialization;
+using FuscaFilmes.API.DbContexts;
+using FuscaFilmes.API.Entities;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Context>(
+    options => options.UseSqlite(builder.Configuration["ConnectionStrings:FuscaFilmesStr"])
+);
+
+// using (var context = new Context())
+// {
+//     context.Database.EnsureCreated();
+// }
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.AllowTrailingCommas = true;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+
+app.MapGet("/diretores", (Context context) =>
+{
+    return context.Diretores.Include(diretor => diretor.Filmes).ToList();
+})
+.WithOpenApi();
+
+app.MapPost("/diretores", (Context context, Diretor diretor) =>
+{
+    context.Diretores.Add(diretor);
+    context.SaveChanges();
+})
+.WithOpenApi();
+
+app.MapPut("/diretores/{diretorId}", (Context context, int diretorId, Diretor diretorNovo) =>
+{
+    var diretor = context.Diretores.Find(diretorId);
+
+    if (diretor != null)
+    {
+        diretor.Name = diretorNovo.Name;
+        if (diretorNovo.Filmes.Count > 0)
+        {
+            diretor.Filmes.Clear();
+            foreach (var filme in diretorNovo.Filmes)
+            {
+                diretor.Filmes.Add(filme);
+            }
+        }
+    }
+
+    context.SaveChanges();
+})
+.WithOpenApi();
+
+app.MapDelete("/diretores/{diretorId}", (Context context, int diretorId) =>
+{
+    var diretor = context.Diretores.Find(diretorId);
+    if (diretor != null)
+        context.Diretores.Remove(diretor);
+
+     context.SaveChanges(); 
+})
+.WithOpenApi();
+
+app.Run();
