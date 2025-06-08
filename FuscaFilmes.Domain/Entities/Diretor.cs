9b6a2749aa1b8c669cac18cb4813eae81c05effa
Guilherme Soares <guicesarsosil@gmail.com>
2025-06-08 13:37:57 -0300

    feat: Data Annotation e Fluent API

--- FuscaFilmes.Domain/Entities/Diretor.cs
+++ FuscaFilmes.Domain/Entities/Diretor.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FuscaFilmes.Domain.Entities;
 
 public class Diretor
 {
+
+  [Key]
+  [Column("id_diretor")]
   public int Id { get; set; }
   public required string Name { get; set; }
   public ICollection<DiretorFilme> DiretoresFilmes { get; set; } = null!;
